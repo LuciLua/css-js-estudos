05738169140a08fa4051a6761f6895f570d209c7
LuciLua <luci.lua81@gmail.com>
2021-05-19 19:02:25 -0300

    Testando forEach | callback :computer:

--- js/app.js
+++ js/app.js
@@ -33,3 +33,15 @@ function tocar(){
 }
 
 document.querySelector('.btn-c').addEventListener('click', function(){document.getElementById('interno').classList.toggle('interno-2')})
+
+
+const lista = ["Canetas", "Estojos", "mochilas", "tesouras", "cadernos"]
+
+function listar(nome, indice){
+ document.getElementById('pl').textContent = `${indice + 1}. ${nome}`
+
+ console.log(`${indice + 1}. ${nome}`)
+}
+
+lista.forEach(listar, listando => console.log(listando) )
+
